ec63686d6e3f4730871ab2658a3b613bb6a7655e
Epoza <86025026+Epoza@users.noreply.github.com>
2024-01-13 23:12:09 -0500

    make message clearer

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,5 +1,5 @@
 export default function helloWorld(element: HTMLDivElement) {
-    element.innerHTML = "hello world";
-    element.style.color = "white";
+    element.innerHTML = "hello world, it is working";
+    element.style.color = "grey";
     element.style.fontSize = "24px";
 }
